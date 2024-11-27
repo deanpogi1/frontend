62b3bd848edfc46d16dd96474a522083b7e728b6
deanpogi1 <dpacardo@gbox.ncf.edu.ph>
2024-11-27 20:03:58 +0800

    mapagalon ka

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";  
+import Home from './Pages/Home';  
 import About from "./Pages/About";  
 import Education from "./Pages/Education"; 
 import Hobbies from "./Pages/Hobbies"; 
