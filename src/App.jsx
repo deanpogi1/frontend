1345acf978dbdb4667a811619911f99d118b3d16
deanpogi1 <dpacardo@gbox.ncf.edu.ph>
2024-11-27 20:12:29 +0800

    rarararararara

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home.jsx';  
-import About from "./Pages/About";  
-import Education from "./Pages/Education"; 
-import Hobbies from "./Pages/Hobbies"; 
-import Contact from "./Pages/Contact";  
-import NavButtons from "./components/NavButtons"; 
+import Home from "./Pages/Home"; // Page component for home
+import About from "./Pages/About"; // Page component for about
+import Education from "./Pages/Education"; // Page component for education
+import Hobbies from "./Pages/Hobbies"; // Page component for hobbies
+import Contact from "./Pages/Contact"; // Page component for contact
+import NavButtons from "./components/NavButtons"; // Navigation bar component
 
 function App() {
   return (
     <Router>
-      <NavButtons /> {/* Navigation Buttons Component */}
+      {/* Navigation Bar */}
+      <NavButtons />
+
       <div className="container">
+        {/* Routes for page navigation */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Education" element={<Education />} />
-          <Route path="/Hobbies" element={<Hobbies />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/" element={<Home />} /> {/* Home Page */}
+          <Route path="/about" element={<About />} /> {/* About Page */}
+          <Route path="/education" element={<Education />} /> {/* Education Page */}
+          <Route path="/hobbies" element={<Hobbies />} /> {/* Hobbies Page */}
+          <Route path="/contact" element={<Contact />} /> {/* Contact Page */}
         </Routes>
       </div>
     </Router>
